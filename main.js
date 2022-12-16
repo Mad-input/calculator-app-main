ebdbb6883c3ca16af8bb92058cb3a3248617429c
Miguel <macc07112005c80@gmail.com>
2022-12-16 01:08:01 -0500

    coree

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const load = () => {
   const storage = localStorage.getItem("BodyTheme");
 
   if (storage == "1" || storage == "2" || storage == "3") {
-    range.value = storage
+    range.value = storage;
     if (storage == "1") {
       body.classList.remove("theme-2", "theme-3");
       body.classList.add("theme-1");
@@ -25,7 +25,6 @@ const load = () => {
       body.classList.add("theme-3");
     }
   } else body.classList.add("theme-1");
-
 };
 
 const storage = (value) => {
@@ -39,26 +38,27 @@ load();
 const controls = document.querySelectorAll(".control");
 const display = document.getElementById("display");
 const btnReset = document.getElementById("reset");
-const btnDel = document.getElementById('del')
+const btnDel = document.getElementById("del");
 const btnEqual = document.getElementById("equal");
 
+btnDel.addEventListener("click", () => {
+  let str = display.innerText;
+  display.innerText = str.substring(0, str.length - 1);
+});
 
-btnDel.addEventListener('click', ()=> {
-  let str = display.innerText
-  display.innerText = str.substring(0, str.length - 1)
-})
+btnReset.addEventListener("click", () => {
+  display.innerHTML = "";
+});
 
-btnReset.addEventListener('click', ()=> {
-  display.innerHTML = '';
-})
-
-btnEqual.addEventListener('click', ()=> {
-  display.innerText = eval(display.innerText)
-})
+btnEqual.addEventListener("click", () => {
+  display.innerText = eval(display.innerText);
+});
 
 controls.forEach((control) => {
   control.addEventListener("click", () => {
     if (control.dataset.type !== "number" && control.dataset.type !== "symbol")
+      return;
+    else if (display.innerText == "" && control.dataset.type !== "number")
       return;
     display.innerHTML += control.innerHTML;
   });
